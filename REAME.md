90c2a15a41f3c1f3dbdca960ceee8225acf2b1eb
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-07-02 16:42:17 -0300

    funcção in ts

---
+++ REAME.md
@@ -0,0 +1,7 @@
+
+
+tsc index.ts
+npm start
+
+open local host:
+http://localhost:1234/
---
+++ index.ts
@@ -0,0 +1,12 @@
+
+{
+    console.log("typeScript");
+    // função
+
+    function tsSoma(a: number, b: number) {
+        return a + b;
+    }
+    const resultTs = tsSoma(1, 2);
+
+    console.log(resultTs);
+}
